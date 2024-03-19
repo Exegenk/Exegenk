dfa254f16bb46f911166bd99755b1500ff929c63
Exegenk <161508186+Exegenk@users.noreply.github.com>
2024-03-19 14:29:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+/* 
+ -- READ THIS --
+
+- You can change the server feature in index.html file and find the feature part / feature area
+- You can change your server about in index.html file and find the about part / about us area
+
+ -- CHANGE THE WEBSITE OPTION IN BELOW --
+*/
+let option = {
+    server_name: "GTPS 28",
+    server_description: "Growtopia Private Server",
+    owner: [
+        "RAP",
+    ],
+    discord_invite_link: "https://chat.whatsapp.com/Ejzsw83X4LO6XvQomhT3J6",
+    ip_address: "159.223.88.58",
+    host_for_android: "https://gtps.fun/gtps28v1.txt"
+}
+
